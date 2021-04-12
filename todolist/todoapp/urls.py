ebdbf709730bd6375850150da3d65d04c00b820f
Kiran K K <kirankalak4@gmail.com>
2021-04-12 08:58:02 -0700

    Update function is now working properly

--- todolist/todoapp/urls.py
+++ todolist/todoapp/urls.py
@@ -3,6 +3,6 @@ from todoapp import views
 
 urlpatterns = [
     path('',views.index,name='home'),
-    path('update/<int:commonid>/',views.update,name=update)
+    path('update/<int:commonid>/',views.update,name='update')
     
 ]
--- todolist/todoapp/views.py
+++ todolist/todoapp/views.py
@@ -16,4 +16,16 @@ def index(request):
     return render(request,'home.html',{'formkey':formobj,'modelobjectkey':modelobject})
 
 def update(request,commonid):
+    modelobj = TodoModel.objects.get(id=commonid)
+    formobj = TodoForm(instance=modelobj)
+
+    if request.method == 'POST':
+        variable=TodoForm(request.POST,instance=modelobj)
+        if variable.is_valid():
+            variable.save()
+            return redirect('home')   
+     
+    return render(request,'update.html',{'formobj':formobj})
+    
+
     
